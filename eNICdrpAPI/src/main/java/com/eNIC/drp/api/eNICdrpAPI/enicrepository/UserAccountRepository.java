41c2aa985c3d1929c8c0b869872c09d7a050c9fb
Divya_Sitinamaluwa <divya.sitinamaluwa@users.noreply.github.com>
2021-05-24 23:47:42 +0530

    root commit

--- eNICdrpAPI/src/main/java/com/eNIC/drp/api/eNICdrpAPI/enicrepository/UserAccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eNIC.drp.api.eNICdrpAPI.enicrepository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.eNIC.drp.api.eNICdrpAPI.enicentity.UserAccount;
-public interface UserAccountRepository extends CrudRepository<UserAccount, Integer> {
-	UserAccount findByUsername(String username);
-}
